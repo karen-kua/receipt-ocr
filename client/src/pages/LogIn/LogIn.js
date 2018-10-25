bf70d5547c8bf81388c27ca0d4ffbc112da0d926
larry1010 <38902578+ll1010@users.noreply.github.com>
2018-10-24 20:10:40 -0400

    LogIn folder added

---
+++ client/src/pages/LogIn/LogIn.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { Col, Row, Container } from "../../components/Grid";
+// import Jumbotron from "../../components/Jumbotron";
+
+class noMatch extends Component {
+render() {
+return (
+
+ 
+          <h1>404 Page Not Found</h1>
+        
+       
+      );
+    }
+}
+export default NoMatch;
