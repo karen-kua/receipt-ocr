9a4f5042c66be10dcdd0a883669f422a3beececc
Durian <karen.kua7@gmail.com>
2018-10-31 18:13:42 -0400

    Edited to fix conflict

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Upload from "./pages/Upload";
 import BrowseBackEnd from "./pages/BrowseBackEnd";
-// import Browse from "./pages/Browse";
+import Browse from "./pages/Browse";
 // import SignIn from "./pages/SignIn";
 // import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
@@ -14,8 +14,8 @@ const App = () => (
       <Switch>
         <Route exact path="/upload" component={Upload} />
         <Route exact path="/test" component={BrowseBackEnd} />
-        {/* <Route exact path="/browse" component={Browse} />
-        <Route exact path="/sign-in" component={SignIn} /> */}
+        <Route exact path="/browse" component={Browse}/>
+        {/* <Route exact path="/sign-in" component={SignIn} /> */} */}
         {/* <Route component={NoMatch} /> */}
       </Switch>
     </div>
