54d9b2222bbf234a414aed3a28f10871e4a1b2f3
larry1010 <38902578+ll1010@users.noreply.github.com>
2018-11-02 16:45:21 -0400

    mongoDB connected

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const usersSchema = new Schema({
-  email: { type: String, required: true },
+  username: { type: String, required: true },
   password: { type: String, required: true },
 });
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,30 @@
+// const db = require("../../models");
+
 const router = require("express").Router();
+const usersController = require("../../controllers/usersController");
+
 
 router.route("/sign-up")
-  .post(k)
+  .post(usersController.create)
 
-function k (req, res) {
-	console.log("users.js hi")
-	// db.Users
-	// .create(req.body)
-	// .then(dbModel => res.json(dbModel))
-	// .catch(err => res.status(422).json(err));
-}
+// function k (req, res) {
+// 	console.log("users.js hi")
+// 	db.Users
+// 	.create(req.body)
+// 	.then(dbModel => res.json(dbModel))
+// 	.catch(err => res.status(422).json(err));
+// }
+
+// router.route("/save")
+//   .post(usersController.create)
+
+// router.route("/:id")
+//   .delete(usersController.remove)
+
+// router.route("/edit/:id")
+//   .get(usersController.findOne);
+
+// router.route("/update/:id")
+//   .put(usersController.update);
 
 module.exports = router;
