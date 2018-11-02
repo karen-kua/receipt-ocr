7a4841df5b722310207bfa3bf7e66fc8a3d992c3
larry1010 <38902578+ll1010@users.noreply.github.com>
2018-11-02 14:09:56 -0400

    Node server connected.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 const expenseRoutes = require("./expense");
+const usersRoutes = require("./users");
+
+// Sign-Up routes
+router.use("/users", usersRoutes);
 
 // Upload routes
 router.use("/expense", expenseRoutes);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,14 @@
-//routing
-app.post('/', (req, res, next)=> {
-	console.log('server post username: ');
-	console.log(req.body.username)
-	res.end()
-})
+const router = require("express").Router();
+
+router.route("/sign-up")
+  .post(k)
+
+function k (req, res) {
+	console.log("users.js hi")
+	// db.Users
+	// .create(req.body)
+	// .then(dbModel => res.json(dbModel))
+	// .catch(err => res.status(422).json(err));
+}
+
+module.exports = router;
