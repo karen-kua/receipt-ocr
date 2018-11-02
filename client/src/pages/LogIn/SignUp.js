4f17f81137a142ce196fb1e70ed3c66eec634e45
larry1010 <38902578+ll1010@users.noreply.github.com>
2018-11-02 00:26:24 -0400

    404 error

--- client/src/pages/LogIn/SignUp.js
+++ client/src/pages/LogIn/SignUp.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
-class SignupForm extends Component {
+class Signup extends Component {
 	constructor() {
 		super()
 		this.state = {
 			username: '',
 			password: '',
 			confirmPassword: '',
-			
+
 		}
 		this.handleSubmit = this.handleSubmit.bind(this)
 		this.handleChange = this.handleChange.bind(this)
@@ -18,40 +19,80 @@ class SignupForm extends Component {
 		})
 	}
 	handleSubmit(event) {
+		console.log('sign-up handleSubmit, username: ')
+		console.log(this.state.username)
 		event.preventDefault()
-        console.log('sign-up-form, username: ');
-        console.log(this.state.username);
-		//request to server here
-	}
-	render() {
 
-		return (
-			<div className="SignupForm">
-				<h1>Signup form</h1>
-				<div>
-				<label htmlFor="username">Username: </label>
-				<input
-					type="text"
-					name="username"
-					value={this.state.username}
-					onChange={this.handleChange}
-				/>
-				</div>
-				<br/>
-				<div>
-				<label htmlFor="password">Password: </label>
-								<input
-									type="password"
-									name="password"
-									value={this.state.password}
-									onChange={this.handleChange}
-								/>
-				</div>
-				<br/>
-				<button onClick={this.handleSubmit}>Sign up</button>
-			</div>
-		)
+		//request to server to add a new username/password
+		axios.post('/user/', {
+			username: this.state.username,
+			password: this.state.password
+		})
+			.then(response => {
+				console.log(response)
+				if (!response.data.errmsg) {
+					console.log('successful signup')
+					this.setState({ //redirect to login page
+						redirectTo: '/login'
+					})
+				} else {
+					console.log('username already taken')
+				}
+			}).catch(error => {
+				console.log('signup error: ')
+				console.log(error)
+
+			})
 	}
+
+
+render() {
+	return (
+		<div className="SignupForm">
+			<h4>Sign up</h4>
+			<form className="form-horizontal">
+				<div className="form-group">
+					<div className="col-1 col-ml-auto">
+						<label className="form-label" htmlFor="username">Username</label>
+					</div>
+					<div className="col-3 col-mr-auto">
+						<input className="form-input"
+							type="text"
+							id="username"
+							name="username"
+							placeholder="Username"
+							value={this.state.username}
+							onChange={this.handleChange}
+						/>
+					</div>
+				</div>
+				<div className="form-group">
+					<div className="col-1 col-ml-auto">
+						<label className="form-label" htmlFor="password">Password: </label>
+					</div>
+					<div className="col-3 col-mr-auto">
+						<input className="form-input"
+							placeholder="password"
+							type="password"
+							name="password"
+							value={this.state.password}
+							onChange={this.handleChange}
+						/>
+					</div>
+				</div>
+				<div className="form-group ">
+					<div className="col-7"></div>
+					<button
+						className="btn btn-primary col-1 col-mr-auto"
+						onClick={this.handleSubmit}
+						type="submit"
+					>Sign up</button>
+				</div>
+			</form>
+		</div>
+
+	)
+}
 }
 
-export default SignupForm
+export default Signup
