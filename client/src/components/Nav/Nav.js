29e4e6020874ab7aba9012de2292ea7719c8e9cc
Alex Jivov <alexjivov@gwu.edu>
2018-11-03 11:55:14 -0400

    navbar complete - links successfully

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,11 +1,21 @@
 import React from "react";
+import './Nav.css'
 
 const Nav = () => (
+
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     <a className="navbar-brand" href="/">
       React Base for Project 3 Expense App
     </a>
+    <a className="nav-bar-links" href="/">Home</a>
+    <a className="nav-bar-links" href="/Browse">Browse</a>
+    <a className="nav-bar-links" href="/LogIn">Login</a>
+    <a className="nav-bar-sign-up" href="/SignUp">Sign Up</a>
+  
+ 
   </nav>
+  
+  
 );
 
 export default Nav;
