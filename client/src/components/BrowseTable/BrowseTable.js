bc59e0fd6df1a5a00c194e93dbda3f4fe3eea4e5
Durian <karen.kua7@gmail.com>
2018-11-05 01:27:55 -0500

    Added some basic styling to the table

--- client/src/components/BrowseTable/BrowseTable.js
+++ client/src/components/BrowseTable/BrowseTable.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Component } from 'react';
-import '../../pages/Browse/Browse.css'
+// import '../../pages/Browse/Browse.css'
+import './BrowseTable.css'
 
 const Table = props => (
-    <div className="data-table">
-        <table>
+    <div className="table-container">
+        <table className="data-table">
             <thead>
                 <tr>
                     <th>Item</th>
